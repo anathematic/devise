4886ba72a21453784d8091a4df5623cf55e91e3b
Thomas <thomas@icdesign.com.au>
2012-08-14 09:22:58 +1000

    Set password and sign in controllers to return only json

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -15,7 +15,7 @@ class Devise::SessionsController < DeviseController
     resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
-    respond_with resource, :location => after_sign_in_path_for(resource)
+    render json: resource
   end
 
   # DELETE /resource/sign_out
